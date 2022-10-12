9286ab8e540bc1d42e417e954365d4f724e9dac4
Derrick Wan <drrckwan.s@gmail.com>
2022-10-11 18:56:35 -0700

    formatted conventions into columns and rows, added dates and location names

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,20 @@
+function createCard(name, description, pictureUrl, starts, ends, location) {
+    return `
+        <div class="card shadow p-3 mb-5 bg-body rounded">
+            <img src="${pictureUrl}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${location}</h6>
+                <p class="card-text">${description}</p>
+            </div>
+            <div class="card-footer">
+                ${starts} - ${ends}
+            </div>
+        </div>
+    `;
+}
+
+
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -10,28 +27,34 @@ window.addEventListener('DOMContentLoaded', async () => {
         } else {
             const data = await response.json();
 
-            const conference = data.conferences[0];
-            const nameTag = document.querySelector('.card-title');
-            nameTag.innerHTML = conference.name;
+            let index = 0;
 
-            const detailUrl = `http://localhost:8000${conference.href}`;
-            const detailResponse = await fetch(detailUrl);
+            for (let conference of data.conferences) {
 
-            if (detailResponse.ok) {
-                const details = await detailResponse.json();
-                console.log("details:::::", details)
+                const detailUrl = `http://localhost:8000${conference.href}`;
+                const detailResponse = await fetch(detailUrl);
 
-                const description = details.conference.description;
-                const descriptionTag = document.querySelector('.card-text');
-                descriptionTag.innerHTML = description;
+                if (detailResponse.ok) {
+                    const details = await detailResponse.json();
+                    const name = details.conference.name;
+                    const description = details.conference.description;
+                    const pictureUrl = details.conference.location.picture_url;
 
-                const imageTag = document.querySelector('.card-img-top');
-                imageTag.src = details.conference.location.picture_url;
+                    const starts = new Date(details.conference.starts).toLocaleDateString();
+                    const ends = new Date(details.conference.ends).toLocaleDateString();
+
+                    const location = details.conference.location.name
+
+                    const html = createCard(name, description, pictureUrl, starts, ends, location);
+
+                    const column = document.querySelector(`#col-${index % 3}`);
+                    column.innerHTML += html;
+
+                    index += 1;
+                }
             }
-
         }
     } catch (e) {
-        // Figure out what to do if an error is raised
+        console.error(e);
     }
-
 });
